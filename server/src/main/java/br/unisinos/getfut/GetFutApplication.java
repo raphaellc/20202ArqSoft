bd5c8ee1bc797cadfcfa9bbfcfe3b23854dc44d2
Edison_Petter <Edison_Petter@Dell.com>
2020-11-22 19:27:34 -0300

    adicionando main

--- server/src/main/java/br/unisinos/getfut/GetFutApplication.java
+++ server/src/main/java/br/unisinos/getfut/GetFutApplication.java
@@ -1,29 +1,13 @@
 package br.unisinos.getfut;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
-public class GetFutApplication {
+@SpringBootApplication
+public class GetfutApplication {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+	public static void main(String[] args) {
+		SpringApplication.run(GetfutApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(GetFutApplication.class, args);
-    }
-
-//    @Bean
-//    public CommandLineRunner demo(JogadorRepository repository) {
-//        return (args) -> {
-//            // save a few customers
-//            repository.save(new JogadorModel("Vinicius", ZonedDateTime.now().minusYears(21), "vinicius@gmail.com"));
-//            repository.save(new JogadorModel("Miguel", ZonedDateTime.now().minusYears(20), "nathaliagmail.com"));
-//
-//            for (JogadorModel jogador : repository.findAll()) {
-//                LOGGER.info(jogador.toString());
-//            }
-//        };
-//    }
 }
--- server/src/test/java/br/unisinos/getfut/BaseGetFutApplicationTest.java
+++ server/src/test/java/br/unisinos/getfut/BaseGetFutApplicationTest.java
@@ -1,9 +1,9 @@
-package br.unisinos.getfut;
-
-import org.assertj.core.api.WithAssertions;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = GetFutApplication.class)
-public abstract class BaseGetFutApplicationTest implements WithAssertions {
-
-}
+//package br.unisinos.getfut;
+//
+//import org.assertj.core.api.WithAssertions;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(classes = GetFutApplication.class)
+//public abstract class BaseGetFutApplicationTest implements WithAssertions {
+//
+//}
